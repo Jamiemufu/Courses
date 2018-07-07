5bf070b141a60a64bf368a0a6e326824d6aa9291
Jamie Evans <jamieevans@itg.co.uk>
2018-07-07 18:30:14 +0100

    initial commit v2 :wq

---
+++ Colt Steele 1/dom.js
@@ -0,0 +1,6 @@
+var p1 = document.getElementById("first");
+var second = document.getElementsByClassName("special");
+var third = document.querySelector("#first");
+var fourth = document.querySelector(".special");
+var fifth = document.getElementsByTagName("p");
+
---
+++ Colt Steele 1/event.js
@@ -0,0 +1,12 @@
+button = document.querySelector("button");
+body = document.getElementsByTagName("body")[0];
+
+button.addEventListener("click", function() {
+	console.log("someone clicked the button?");
+	if (body.style.background = "red") {
+		body.style.background = "white";
+	} else {
+		body.style.background = "red";
+	}
+
+});
