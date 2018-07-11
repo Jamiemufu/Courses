4cae9a0c39506b1b1e3d38a7446e281190ce6cb9
Jamie Evans <jamieevans@itg.co.uk>
2018-07-11 01:34:53 +0100

    bedtime

--- Colt Steele 1/event.js
+++ Colt Steele 1/event.js
@@ -1,12 +1,11 @@
-button = document.querySelector("button");
-body = document.getElementsByTagName("body")[0];
+var button = document.querySelector('button')
+var body = document.getElementsByTagName('body')[0]
 
-button.addEventListener("click", function() {
-	console.log("someone clicked the button?");
-	if (body.style.background = "red") {
-		body.style.background = "white";
-	} else {
-		body.style.background = "red";
-	}
-
+button.addEventListener('click', function () {
+  console.log('someone clicked the button?')
+  if (body.style.background = 'red') {
+    body.style.background = 'white'
+  } else {
+    body.style.background = 'red'
+  }
 });
