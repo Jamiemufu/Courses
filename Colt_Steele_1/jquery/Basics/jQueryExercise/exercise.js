0561c49602e74b15c2e8a100ff77688c5b265b92
Jamie Evans <taichi.chikung@gmail.com>
2018-09-29 19:30:10 +0100

    exercise completed

---
+++ Colt_Steele_1/jquery/Basics/jQueryExercise/exercise.js
@@ -0,0 +1,15 @@
+// select all divs and give them purple background
+
+$("div").css("background", "purple");
+
+// select all divs with class "highlight"
+
+$(".highlight").css("width", "200px");
+
+//select div with id third and give orange border
+
+$("#third").css("border", "3px solid orange");
+
+//select first div ONLY and change its font color to pink
+
+$("div:first").css("color", "pink");
