b8243d7aa369462db0007522955447f0fc4396fd
Jamie Evans <taichi.chikung@gmail.com>
2018-10-01 21:58:03 +0100

    Extra added

--- Colt_Steele_1/backend/webdevbootcamp/IntroToExpress/FirstExpressApp/app.js
+++ Colt_Steele_1/backend/webdevbootcamp/IntroToExpress/FirstExpressApp/app.js
@@ -5,6 +5,7 @@ var app = express();
 // "/bye" => "Good Bye!"
 // "/dog" => "MEOW!"
 
+
 app.get("/", (req,res) => {
     res.send("Hi there!");
 });
@@ -13,10 +14,24 @@ app.get("/bye", (req,res) => {
     res.send("Good Bye!");
 });
 
-app.get('/dog', (req,res) => {
+app.get("/dog", (req,res) => {
     res.send("MEOW!!");
-    
-    
+});
+//SemiColon is a route variable
+app.get('/r/:subredditName', (req, res) => {
+    //params, value
+    console.log(req);
+    var subreddit = req.params.subredditName;
+    res.send("Welcome to a" + subreddit);
+});
+
+app.get('/r/:subredditName/comments/:id/:title/', (req, res) => {
+    res.send("Welcome to the comments page");
+});
+
+//catch all routes not designated (order matters)
+app.get("*", (req, res) => {
+    res.send("You are a star");
 });
 
 //tell express to listen for requests (start server)
