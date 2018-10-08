cdc3d3ac6785beb955b2caed90f0434bca4200a1
Jamie Evans <jamieevans@itg.co.uk>
2018-10-08 15:51:56 +0100

    amends eslint

--- Colt_Steele_1/backend/webdevbootcamp/moreExpress/PostRequestDemo/app.js
+++ Colt_Steele_1/backend/webdevbootcamp/moreExpress/PostRequestDemo/app.js
@@ -1,44 +1,39 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
 
-var pets = [
-    "Cats",
-    "Dogs",
-    "Birds",
-    "Fish"
-];
+var pets = ['Cats', 'Dogs', 'Birds', 'Fish']
 
-//tell express to use body document -- copy paste job
-app.use(bodyParser.urlencoded({extended: true}));
+// tell express to use body document -- copy paste job
+app.use(bodyParser.urlencoded({ extended: true }))
 
-//tell app to use public dir
-app.use(express.static("public"));
-//set template engine to ejs
-app.set("view engine", "ejs");
+// tell app to use public dir
+app.use(express.static('public'))
+// set template engine to ejs
+app.set('view engine', 'ejs')
 
-//default route
+// default route
 app.get('/', (req, res) => {
-    res.render("home");
-});
+  res.render('home')
+})
 
 app.post('/addPet', (req, res) => {
-    var newPet = req.body.petname;
-    pets.push(newPet);
-    //redirect to give illusion or refresh /pets
-     res.redirect('/pets');
-});
+  var newPet = req.body.petname
+  pets.push(newPet)
+  // redirect to give illusion or refresh /pets
+  res.redirect('/pets')
+})
 
 app.get('/pets', (req, res) => {
-    res.render("pets", {pets: pets});
-});
+  res.render('pets', { pets: pets })
+})
 
-//not found route !NEEDS TO BE BOTTOM ROUTE!
+// not found route !NEEDS TO BE BOTTOM ROUTE!
 app.get('*', (req, res) => {
-    res.send("Sorry the page you are requesting is not available");
-});
+  res.send('Sorry the page you are requesting is not available')
+})
 
-//set app to listen on 3000
+// set app to listen on 3000
 app.listen(3000, () => {
-    console.log('App listening on port 3000!');
-});
+  console.log('App listening on port 3000!')
+})
