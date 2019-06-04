e80a8689a7bd142305bb5e32559a7e9005e86aa3
Jamie Evans <jamieevans@itg.co.uk>
2019-06-04 15:52:24 +0100

    monster_slayer app completed

--- vue/monster_slayer/js/app.js
+++ vue/monster_slayer/js/app.js
@@ -4,46 +4,65 @@ new Vue({
         playerHealth: 100,
         enemyHealth: 100,
         gameRunning: false,
+        logs: [],
     },
     methods: {
         startGame: function () {
             this.gameRunning = true
             this.playerHealth = 100
             this.enemyHealth = 100
+            this.logs = []
         },
         attack: function () {
-            this.enemyHealth -= this.calcDamage(3, 10)
+            let damage = this.calcDamage(3, 10)
+            this.enemyHealth -= damage
+
+            this.addLogs('Player hits for', true, damage)
+
             if(this.checkWin()) {
                 return
             }
 
-            this.playerHealth -= this.calcDamage(4,9)
+            damage = this.calcDamage(3, 10)
+            this.playerHealth -= damage
+
+            this.addLogs('Monster hits for', false, damage)
+
             if(this.checkWin()) {
                 return
             }
         },
         specialAttack: function () {
-            this.enemyHealth -= this.calcDamage(6, 20)
+            let damage = this.calcDamage(6, 20)
+            this.enemyHealth -= damage
+
+            this.addLogs('Player special attacks for', true, damage)
+
             if(this.checkWin()) {
                 return
             }
+            damage = this.calcDamage(9, 18)
+            this.playerHealth -= damage
 
-            this.playerHealth -= this.calcDamage(9, 18)
+            this.addLogs('Monster special attacks for', false, damage)
+
             if(this.checkWin()) {
                 return
             }
         },
-        heal: function () {
+        heal: function () {            
             if (this.playerHealth >= 95) {
                 this.playerHealth = this.playerHealth
             } else if (this.playerHealth < 95) {
                 this.playerHealth += 10
+                this.addLogs('Player heals for', true, 10)
             }
         },
         giveUp: function () {
             this.playerHealth = 100
             this.enemyHealth = 100
             this.gameRunning = false
+            this.logs = []
         },
         calcDamage: function (min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min)
@@ -65,6 +84,12 @@ new Vue({
                 return true
             }
             return false
+        },
+        addLogs: function(message, player, action) {
+            this.logs.unshift({
+                isPlayer: player,
+                text: message + ' ' + action
+            })
         }
 
     }
