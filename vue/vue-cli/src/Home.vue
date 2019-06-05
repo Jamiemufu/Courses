0b8d6da86503db9ba229e045237d6cc4f75f88e8
Jamie Evans <jamie@jedesign.xyz>
2019-06-06 00:18:06 +0100

    first vue cli

---
+++ vue/vue-cli/src/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- must be wrapped under one root element -->
+    <div>
+        <app-server-status v-bind:key="server" v-for="server in 5"></app-server-status>
+    </div>
+</template>
+
+<script>
+    // local components
+    import ServerStatus from './ServerStatus.vue'
+    export default {
+        // components property
+        components: {
+            'app-server-status': ServerStatus
+        }
+    }
+</script>
+
---
+++ vue/vue-cli/src/ServerStatus.vue
@@ -0,0 +1,26 @@
+<template>
+    <!-- must be wrapped under one root element -->
+    <!-- this will be rendered inside home -->
+    <div>
+        <p>Server Status: {{ status }}</p>
+        <hr>
+        <button @click="changeStatus">Change Status</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        // es6 way data() { return {} }
+        data: function() {
+            return {
+                status: 'Critical'
+            }
+        },
+        methods: {
+            changeStatus() {
+                this.status = 'Normal'
+            }
+        }
+    }
+</script>
+
